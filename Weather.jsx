9c3409af01407e0b90f561573ed607160330977b
BHARATH KUMAR <sudeepbharath66@gmail.com>
2025-04-03 12:37:56 +0530

    Update Weather.jsx

--- Weather.jsx
+++ Weather.jsx
@@ -1,5 +1,5 @@
 /*import React, { useEffect, useState } from 'react';
-import './Weather.css'; // Ensure correct spelling
+import './Weather.css'; 
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
 import cloud_icon from '../assets/cloud.png'
